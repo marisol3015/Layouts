05f51f94bfda08a531453f9a2ec3fc5ba15c6fc8
mary3015 <mary.3015@hotmail.com>
2016-03-21 00:02:43 -0500

    Initial commit

---
+++ app/src/main/java/com/uniquindio/electiva_android/layouts/ManagerLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.uniquindio.electiva_android.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout_activity);
+    }
+}
